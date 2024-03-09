b67f99cf68291aaf31d51baef7d96bc6879b83df
Leticia Alves <130405282+LeticiaOk@users.noreply.github.com>
2024-03-09 01:43:56 -0300

    Add files via upload

---
+++ exercicios/ex006.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+
+print('O dobro de {} vale {}.'.format((n), (n*2)))
+print('O triplo de {} vale {}.\nA raiz qaudrada de {} é igual a {:.2f}'.format(n, (n*3), n, pow(n, (1/2))))
